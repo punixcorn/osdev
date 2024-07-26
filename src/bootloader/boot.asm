88f27c6499615b80e180b53bf5d1f913809b00d9
punixcorn <ohyunwoods663@gmail.com>
2024-07-26 15:12:01 +0000

    minor changes made

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -1,7 +1,6 @@
 bits 16
 [org 0x7c00]
 
-
 	mov  si, MSG_REAL_MODE
 	call print
 	hlt
@@ -15,15 +14,15 @@ bits 16
 	mov  si, MSG_PROTECTED_MODE
 	call print
 
-    ; we jump to CODE_SEG:pm_start
+	;    we jump to CODE_SEG:pm_start
 	call enter_protected
 
 	jmp $
 
-%include "src/include/disk_read.asm"
-%include "src/include/print.asm"
-%include "src/include/gdt.asm"
-%include "src/include/enter_protected.asm"
+%include "src/bootloader/include/disk_read.asm"
+%include "src/bootloader/include/print.asm"
+%include "src/bootloader/include/gdt.asm"
+%include "src/bootloader/include/enter_protected.asm"
 
 MSG_PROTECTED_MODE: db 0x0d,0x0a,"Moving into protected Mode",0x0d,0x0a,0
 MSG_REAL_MODE: db "Real Mode: Welcome to potatOs",0x0d,0x0a,0
@@ -36,8 +35,8 @@ pm_start:
 	;mov   dword [0xb8000], 0x2f4b2f4f
 
 	;Jump to kernel code by calling memory location
-	call KERNEL_OFFSET
-	jmp  $
+	call  KERNEL_OFFSET
+	jmp   $
 
 times 510-($-$$) db 0
 dw    0xaa55
